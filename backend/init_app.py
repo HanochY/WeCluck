6a36712c200f00eea3c803200b6753fb1cb65ea5
hanochyarkoni <hanochyarkoni@gmail.com>
2024-04-24 11:15:00 +0300

    fixsd config

--- backend/init_app.py
+++ backend/init_app.py
@@ -1,4 +1,6 @@
-
+import secrets
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from config.app import app
 
 from routes.comments import comments_blueprint
@@ -14,7 +16,15 @@ app.register_blueprint(preflight_blueprint)
 app.register_blueprint(authentication_blueprint)
 app.register_blueprint(topics_blueprint)
 
+
 with app.app_context():
+    
+    database = SQLAlchemy()
+    app = Flask(__name__, template_folder='../templates', static_folder='../static')
+    app.secret_key = secrets.token_urlsafe(32)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DBNAME}.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    database.init_app(app)
     init_db()
     app.run(debug=True)
     
