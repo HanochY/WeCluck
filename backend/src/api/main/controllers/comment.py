9ff8212e26bd1d967fa9a166dae618ce79f89848
hanochyarkoni <hanochyarkoni@gmail.com>
2024-12-16 16:01:33 +0200

    small fix

--- backend/src/api/main/controllers/comment.py
+++ backend/src/api/main/controllers/comment.py
@@ -23,7 +23,7 @@ class CommentController(Controller[Comment, CommentModels.Create, CommentModels.
                 
         except TypeError as e:
             print(e)
-            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Item not found")
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity!")
         
         if object: return object.id 
         else: return None
@@ -47,9 +47,12 @@ class CommentController(Controller[Comment, CommentModels.Create, CommentModels.
         else: return None
     
     async def update(self, id: UUID, new_data: CommentModels.Update) -> None:
-        async for session in get_db_session():
-            await self.repository.update(id=id, session=session, **new_data)
-        return None
+        try:
+            async for session in get_db_session():
+                await self.repository.update(id=id, session=session, **new_data)
+            return None
+        except TypeError:
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity!")
     
     async def delete(self, id: UUID) -> None:
         async for session in get_db_session():
--- backend/src/api/main/controllers/user.py
+++ backend/src/api/main/controllers/user.py
@@ -23,7 +23,7 @@ class UserController(Controller[User, UserModels.Create, UserModels.Update, User
                 
         except TypeError as e:
             print(e)
-            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Item not found")
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity!")
         
         if object: return object.id 
         else: return None
@@ -47,9 +47,12 @@ class UserController(Controller[User, UserModels.Create, UserModels.Update, User
         else: return None
     
     async def update(self, id: UUID, new_data: UserModels.Update) -> None:
-        async for session in get_db_session():
-            await self.repository.update(id=id, session=session, **new_data)
-        return None
+        try:
+            async for session in get_db_session():
+                await self.repository.update(id=id, session=session, **new_data)
+            return None
+        except TypeError:
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable entity!")
     
     async def delete(self, id: UUID) -> None:
         async for session in get_db_session():
